295d9c041131b9320f3df60a62edb96f7ee38b73
Rahul Kohli <kohlirahul23@gmail.com>
2021-09-22 15:52:20 +0530

    reverse deployment attempt to heroku

--- server/index.js
+++ server/index.js
@@ -9,18 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(compression);
-	app.use(express.static(path.join(__dirname,'client/build')));
-    app.get('*', (request, response) => {
-    	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-
-app.listen(port, error => {
-    if (error) throw error;
-    console.log('server running on port ' + port);
-})
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 
 app.get('/username/compliance-check/:username',
 check('username').matches(/^[a-zA-Z][a-zA-Z][0-9][0-9][a-zA-Z][a-zA-Z]$/).isLength(6), (req, res) => {
@@ -31,4 +21,10 @@ check('username').matches(/^[a-zA-Z][a-zA-Z][0-9][0-9][a-zA-Z][a-zA-Z]$/).isLeng
         return res.status(400).json({ errors: errors.array() });
     }
     res.status(200).json({message: 'ok'})
+})
+
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('server running on port ' + port);
 })
